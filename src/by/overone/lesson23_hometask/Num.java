acf1f8c5b8f3489054ca35a2ca5fdbcf896f7d24
RGNRK80 <armhound@gmail.com>
2021-11-08 15:18:57 +0300

    hometask 23 added  || rezult MIN_VALUE

--- src/by/overone/lesson23_hometask/Num.java
+++ src/by/overone/lesson23_hometask/Num.java
@@ -12,33 +12,33 @@ public class Num < T extends Number>{
 
     public double multilpikate (double x) {
         var rezult=val.doubleValue() * x;
-        if (rezult>Double.MAX_VALUE) {throw new ArithmeticException("double overflow");}
+        if (rezult>Double.MAX_VALUE || rezult<Double.MIN_VALUE) {throw new ArithmeticException("double overflow");}
         return (double) rezult;
     }
 
     public  float multilpikate (float x) {
         var rezult=val.doubleValue() * x;
-        if (rezult>Float.MAX_VALUE) {throw new ArithmeticException("float overflow");}
+        if (rezult>Float.MAX_VALUE || rezult<Float.MIN_VALUE) {throw new ArithmeticException("float overflow");}
         return (float) rezult;
     }
     public  int multilpikate (int x) {
         var rezult= val.doubleValue() * x;
-        if (rezult>Integer.MAX_VALUE) {throw new ArithmeticException("integer overflow");}
+        if (rezult>Integer.MAX_VALUE || rezult<Integer.MIN_VALUE) {throw new ArithmeticException("integer overflow");}
         return (int) rezult;
     }
     public  long multilpikate (long x) {
         var rezult= val.doubleValue() * x;
-        if (rezult>Long.MAX_VALUE) {throw new ArithmeticException("long overflow");}
+        if (rezult>Long.MAX_VALUE || rezult<Long.MIN_VALUE) {throw new ArithmeticException("long overflow");}
         return (long) rezult;
     }
     public  short multilpikate (short x) {
         var rezult=val.doubleValue() * x;
-        if (rezult>Short.MAX_VALUE) {throw new ArithmeticException("short overflow");}
+        if (rezult>Short.MAX_VALUE || rezult<Short.MIN_VALUE) {throw new ArithmeticException("short overflow");}
         return (short) rezult;
     }
     public  byte multilpikate (byte x) {
         var rezult=val.doubleValue() * x;
-        if (rezult>Byte.MAX_VALUE) {throw new ArithmeticException("byte overflow");}
+        if (rezult>Byte.MAX_VALUE  || rezult<Byte.MIN_VALUE) {throw new ArithmeticException("byte overflow");}
         return (byte) rezult;
     }
 
@@ -48,33 +48,33 @@ public class Num < T extends Number>{
 
     public double division (double x) {
         var rezult=val.doubleValue() / x;
-        if (rezult>Double.MAX_VALUE) {throw new ArithmeticException("double overflow");}
+        if (rezult>Double.MAX_VALUE  || rezult<Double.MIN_VALUE) {throw new ArithmeticException("double overflow");}
         return (double) rezult;
     }
 
     public  float division (float x) {
         var rezult=val.doubleValue() / x;
-        if (rezult>Float.MAX_VALUE) {throw new ArithmeticException("float overflow");}
+        if (rezult>Float.MAX_VALUE  || rezult<Float.MIN_VALUE) {throw new ArithmeticException("float overflow");}
         return (float) rezult;
     }
     public  int division (int x) {
         var rezult= val.doubleValue() / x;
-        if (rezult>Integer.MAX_VALUE) {throw new ArithmeticException("integer overflow");}
+        if (rezult>Integer.MAX_VALUE || rezult<Integer.MIN_VALUE) {throw new ArithmeticException("integer overflow");}
         return (int) rezult;
     }
     public  long division (long x) {
         var rezult= val.doubleValue() / x;
-        if (rezult>Long.MAX_VALUE) {throw new ArithmeticException("long overflow");}
+        if (rezult>Long.MAX_VALUE || rezult<Long.MIN_VALUE) {throw new ArithmeticException("long overflow");}
         return (long) rezult;
     }
     public  short division (short x) {
         var rezult=val.doubleValue() / x;
-        if (rezult>Short.MAX_VALUE) {throw new ArithmeticException("short overflow");}
+        if (rezult>Short.MAX_VALUE || rezult<Short.MIN_VALUE) {throw new ArithmeticException("short overflow");}
         return (short) rezult;
     }
     public  byte division (byte x) {
         var rezult=val.doubleValue() / x;
-        if (rezult>Byte.MAX_VALUE) {throw new ArithmeticException("byte overflow");}
+        if (rezult>Byte.MAX_VALUE || rezult<Byte.MIN_VALUE) {throw new ArithmeticException("byte overflow");}
         return (byte) rezult;
     }
 
@@ -82,70 +82,67 @@ public class Num < T extends Number>{
 
     public double addTo (double x) {
         var rezult=val.doubleValue() + x;
-        if (rezult>Double.MAX_VALUE) {throw new ArithmeticException("double overflow");}
+        if (rezult>Double.MAX_VALUE || rezult<Double.MIN_VALUE) {throw new ArithmeticException("double overflow");}
         return (double) rezult;
     }
 
     public  float addTo (float x) {
         var rezult=val.doubleValue() + x;
-        if (rezult>Float.MAX_VALUE) {throw new ArithmeticException("float overflow");}
+        if (rezult>Float.MAX_VALUE || rezult<Float.MIN_VALUE) {throw new ArithmeticException("float overflow");}
         return (float) rezult;
     }
 
     public  int addTo (int x) {
         var rezult= val.doubleValue() + x;
-        if (rezult>Integer.MAX_VALUE) {throw new ArithmeticException("integer overflow");}
+        if (rezult>Integer.MAX_VALUE || rezult<Integer.MIN_VALUE) {throw new ArithmeticException("integer overflow");}
         return (int) rezult;
     }
     public  long addTo (long x) {
         var rezult= val.doubleValue() + x;
-        if (rezult>Long.MAX_VALUE) {throw new ArithmeticException("long overflow");}
+        if (rezult>Long.MAX_VALUE || rezult<Long.MIN_VALUE) {throw new ArithmeticException("long overflow");}
         return (long) rezult;
     }
     public  short addTo (short x) {
         var rezult=val.doubleValue() + x;
-        if (rezult>Short.MAX_VALUE) {throw new ArithmeticException("short overflow");}
+        if (rezult>Short.MAX_VALUE || rezult<Short.MIN_VALUE) {throw new ArithmeticException("short overflow");}
         return (short) rezult;
     }
     public  byte addTo (byte x) {
         var rezult=val.doubleValue() + x;
-        if (rezult>Byte.MAX_VALUE) {throw new ArithmeticException("byte overflow");}
+        if (rezult>Byte.MAX_VALUE || rezult<Byte.MIN_VALUE) {throw new ArithmeticException("byte overflow");}
         return (byte) rezult;
     }
 
-
-
-
     public double subtraction (double x) {
         var rezult=val.doubleValue() - x;
-        if (rezult>Double.MAX_VALUE) {throw new ArithmeticException("double overflow");}
+        if (rezult>Double.MAX_VALUE || rezult<Double.MIN_VALUE) {throw new ArithmeticException("double overflow");}
         return (double) rezult;
     }
 
     public  float subtraction (float x) {
         var rezult=val.doubleValue() - x;
-        if (rezult>Float.MAX_VALUE) {throw new ArithmeticException("float overflow");}
+        if (rezult>Float.MAX_VALUE || rezult<Float.MIN_VALUE) {throw new ArithmeticException("float overflow");}
         return (float) rezult;
     }
 
     public  int subtraction (int x) {
         var rezult= val.doubleValue() - x;
-        if (rezult>Integer.MAX_VALUE) {throw new ArithmeticException("integer overflow");}
+        if (rezult>Integer.MAX_VALUE || rezult<Integer.MIN_VALUE) {throw new ArithmeticException("integer overflow");}
         return (int) rezult;
     }
     public  long subtraction (long x) {
         var rezult= val.doubleValue() - x;
-        if (rezult>Long.MAX_VALUE) {throw new ArithmeticException("long overflow");}
+        if (rezult>Long.MAX_VALUE || rezult<Long.MIN_VALUE) {throw new ArithmeticException("long overflow");}
         return (long) rezult;
     }
     public  short subtraction (short x) {
         var rezult=val.doubleValue() - x;
-        if (rezult>Short.MAX_VALUE) {throw new ArithmeticException("short overflow");}
+        if (rezult>Short.MAX_VALUE || rezult<Short.MIN_VALUE) {throw new ArithmeticException("short overflow");}
         return (short) rezult;
     }
     public  byte subtraction (byte x) {
         var rezult=val.doubleValue() - x;
-        if (rezult>Byte.MAX_VALUE) {throw new ArithmeticException("byte overflow");}
+        if (rezult>Byte.MAX_VALUE || rezult<Byte.MIN_VALUE) {throw new ArithmeticException("byte overflow");}
         return (byte) rezult;
     }
 }
